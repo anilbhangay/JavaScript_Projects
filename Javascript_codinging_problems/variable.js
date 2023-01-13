784f2ffbd84ef21a2e37173ed7406f23a49ff545
anilbhangay <anilbhangay@gmail.com>
2023-01-13 23:13:46 +0530

    Write Data types values

--- Javascript_codinging_problems/variable.js
+++ Javascript_codinging_problems/variable.js
@@ -1,5 +1,19 @@
 // 01.declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
 
- let place = hyderabad;
+let person = "Anmol kumar";
+console.log(person); // string
 
-  console.log(place);
+         //booleans
+let x = "age";
+console.log(18);
+
+if(x > 20) { 
+        console.log("you are eligible for license"); //True
+}
+else { 
+    console.log("you are not eligible"); //False
+}
+
+let bike;
+console.log(bike);  //undefined
+
