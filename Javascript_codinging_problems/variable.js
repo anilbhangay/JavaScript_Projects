9b1ca25473455c73adb8859d3d15172753777ea1
anilbhangay <anilbhangay@gmail.com>
2023-01-19 00:15:32 +0530

    Added some problem in js

--- Javascript_codinging_problems/variable.js
+++ Javascript_codinging_problems/variable.js
@@ -167,8 +167,7 @@ console.log(pythonlength !== jargonlength); //falsy
     - What is the date today?
     - What is the day today as a number?
     - What is the hours now?
-    - What is the minutes now?
-    - Find out the numbers of seconds elapsed from January 1, 1970 to now.*/
+    - What is the minutes now?*/
 
  let now = new Date();
  console.log(now.getFullYear()); //year today
@@ -179,4 +178,105 @@ console.log(pythonlength !== jargonlength); //falsy
  console.log(current); //month today
 
 
+ let dt = new Date();
+ let st = dt.getDate();
+ console.log(st); //date today
+
+
+ let dy = new Date();
+ let de = dy.getDay();
+ console.log(de); //day today as a number
+
+
+ let hrs = new Date();
+ let hr = hrs.getHours();
+ console.log(hr); //hours now
+
+
+ let min = new Date();
+ let mn = min.getMinutes();
+ console.log(mn); //minutes now
+
+// - Find out the numbers of seconds elapsed from January 1, 1970 to now.
+
+
+  let sec = new Date();
+  let sc = (sec.getTime() / 1000);
+  console.log(sc); //seconds now
+
+
+  /*12. Create a human readable time format using the Date time object
+    - YYYY-MM-DD HH:mm
+    - DD-MM-YYYY HH:mm
+    - DD/MM/YYYY HH:mm */
  
+    var ds = new Date();
+    var curr_year1 = ds.getFullYear();
+    var curr_month2 = (ds.getMonth()+1);
+    var curr_date3 = ds.getDate();
+    var curr_time4 = ds.toLocaleTimeString();
+    console.log(curr_year1 + "-" + curr_month2 + "-" + curr_date3 + " " + curr_time4); //YYYY-MM-DD HH:mm
+ 
+
+    var da = new Date();
+    var curr_date = da.getDate();
+    var curr_month = (da.getMonth()+1);
+    var curr_year = da.getFullYear();
+    var curr_time = da.toLocaleTimeString();
+    console.log(curr_date + "-" + curr_month + "-" + curr_year + " " + curr_time); //DD-MM-YYYY HH:mm
+
+    var dj = new Date,     
+    dformat = [dj.getDate(),
+               dj.getMonth()+1,
+               dj.getFullYear()].join('/')+' '+
+              [dj.toLocaleTimeString()];
+               console.log(dformat); //DD/MM/YYYY HH:mm
+
+
+  //  13. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
+
+   let Age = prompt("Enter your age:");
+     
+   if(Age >= 18){
+    console.log('You are old enough to drive');
+   }
+    else{
+      console.log('Wait for the number of years needs to turn 18'); 
+    }  // using prompt  
+
+
+  //14. Even numbers are divisible by 2 and the remainder is zero. How do you check, if a number is even or not using JavaScript? Create a program which checks that the given number is even or odd.
+
+   let num = 8;
+
+   if (num % 2 === 0 ){
+          console.log(num + " is an even number.");
+   }
+     else {
+        console.log(num + " is an odd number.");
+     }    // 8 is an even number
+
+  
+    function EvenorOdd(numr){
+      if(numr % 2 === 0) {
+            console.log(numr + " is an even number.");
+      } 
+        else {
+          console.log(numr + " is an odd number.");
+        }
+    }
+      console.log(EvenorOdd(10)); //even number
+      console.log(EvenorOdd(9)); //odd number
+
+
+  //15. Write a program which can give grades to students according to theirs scores:
+      
+     let score = 100 ;
+     console.log(88);
+
+     if(score <= 80){
+           console.log("Grade - 'A'")
+     }
+       else {
+              console.log("Grade - 'F'");
+       }
