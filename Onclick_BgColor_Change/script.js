5ed6bdb9fe8b5257003d5366b1f9546852d3d6b6
anilbhangay <anilbhangay@gmail.com>
2023-01-26 17:40:56 +0530

    Fixed somer error in js

--- Onclick_BgColor_Change/script.js
+++ Onclick_BgColor_Change/script.js
@@ -1,21 +1,20 @@
 
-
 let btn = document.getElementById("butn");
-
 
 let randomcolor = () => {
     let val = "0123456789ABCDEF";
     let hex = "#";
-     for (let i = 0; i < 6; i++) {
-          hex = hex + val[Math.floor(Math.random() * 16)];
-     }
-     return hex;
+
+    for(let i = 0; i < 6; i++) {
+       hex = hex + val[Math.floor(Math.random() * 16)];
+    }
+      return hex;
 };
+   console.log(randomcolor());
 
-console.log(randomcolor());
 
- function changerandomcolor() {
+   function changerandomcolor() {
       document.body.style.backgroundColor = randomcolor();
  }
 
- btn.addEventListener('click', changerandomcolor);
+   btn.addEventListener('click', changerandomcolor);
