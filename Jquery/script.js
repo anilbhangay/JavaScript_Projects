5566bf6b15b7423ef142d7c94017c992f88b9aae
anilbhangay <anilbhangay@gmail.com>
2023-07-31 23:35:21 +0530

    Used text, html, val method in js

--- Jquery/script.js
+++ Jquery/script.js
@@ -8,17 +8,34 @@
 // });
 
 
-$(document).ready(function(){
-   $("#btn1").click(function(){
-    $("#box").animate({
-      "background-color": "yellow",
-       height: "250px",
-       width: "250px",
-       "margin-left": "280px",
-       "margin-top": "280px",
-    }, 4000, function(){alert("Animate Completed")});
-   });
-       $("#btn2").click(function(){
-         $("#box").stop();
+// $(document).ready(function(){
+//    $("#btn1").click(function(){
+//     $("#box").animate({
+//       "background-color": "yellow",
+//        height: "250px",
+//        width: "250px",
+//        "margin-left": "280px",
+//        "margin-top": "280px",
+//     }, 4000, function(){alert("Animate Completed")});
+//    });
+//        $("#btn2").click(function(){
+//          $("#box").stop();
+//        });
+// });
+
+
+ $(document).ready(function(){
+     $(".btn1").click(function(){
+       alert("Text : " + $(".para1").text());
+     });
+
+       $(".btn2").click(function(){
+         alert("HTML : " + $(".para2").html());
        });
-});
+
+         $(document).ready(function(){
+           $(".btn3").click(function(){
+             alert("Value : " + $(".search").val());
+           });
+         });
+ });
