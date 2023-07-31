52f276a4151bdcaccd45af7b40a36363149eaeb3
anilbhangay <anilbhangay@gmail.com>
2023-07-31 21:12:44 +0530

    Added css in animate function in js

--- Jquery/script.js
+++ Jquery/script.js
@@ -1,10 +1,21 @@
-$(document).ready(function(){ 
-  $("#btn1").click(function(){
-    $("#box").animate({left: "250px"}, 3000, function(){alert("Animation Completed");});
-  });
-     $("#btn2").click(function(){
-        $("#box").stop();
-     });
+// $(document).ready(function(){ 
+//   $("#btn1").click(function(){
+//     $("#box").animate({left: "250px"}, 3000, function(){alert("Animation Completed");});
+//   });
+//      $("#btn2").click(function(){
+//         $("#box").stop();
+//      });
+// });
+
+
+$(document).ready(function(){
+   $("#btn1").click(function(){
+    $("#box").animate({
+       height: "250px",
+       width: "250px",
+       "background-color": 'blue',
+       "margin-left": "280px",
+       "margin-top": "280px",
+    }, 3000);
+   });
 });
-
-
