eb8dd017241fa6b16ba8296406408427f41fbb12
anilbhangay <anilbhangay@gmail.com>
2023-08-09 23:43:45 +0530

    Used High order function in js

--- Jquery/script2.js
+++ Jquery/script2.js
@@ -1,8 +1,28 @@
 // HOF
 
-  const numbers = [1,2,3,4,5];
-    function addone(array){
-        for(let i = 0; i < array.length; i++){
-        }
-    }
-      addone(numbers);
+//   const numbers = [1,2,3,4,5];
+//     function addone(array){
+//         for(let i = 0; i < array.length; i++){
+//         }
+//         console.log(numbers);
+
+//     }
+//       addone(numbers);
+
+
+// Arrow Function
+   //   const sam = () => {
+   //     return "Say Hello!";
+   //   };
+   //     function pam(){
+   //         return sam();
+           
+   //       }
+   //     document.write(pam());
+
+   //   callback function 
+       
+
+
+
+         
